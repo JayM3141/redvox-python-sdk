8c4eaa4b83f987f2ce32ff551466d202af486972
Jay M <jay.mcarthur@protonmail.com>
2026-09-20 07:53:59 +0100

    chore: integrate Sentry SDK for error tracking

--- redvox_gui/redvox_gui/settings.py
+++ redvox_gui/redvox_gui/settings.py
@@ -105,3 +105,19 @@ CACHES = {
         }
     }
 }
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_dsn = os.getenv("SENTRY_DSN")
+if sentry_dsn:
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        profiles_sample_rate=1.0,
+    )
